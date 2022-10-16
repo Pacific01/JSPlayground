c2b1f1d14ef04b76f18b26520e84283f7ca432a4
Jaime Pascual Torres <jpascual@everlyrusher.com>
2022-10-16 15:36:43 +0200

    Basic evaluation

--- index.ts
+++ index.ts
@@ -5,13 +5,14 @@ import { dracula } from '@uiw/codemirror-theme-dracula'
 import { javascript } from "@codemirror/lang-javascript"
 
 const extensions = [
-    dracula,
-    javascript(),
-    lineNumbers(),
-    keymap.of([indentWithTab]),
+  dracula,
+  javascript(),
+  lineNumbers(),
+  keymap.of([indentWithTab]),
 ]
 
 const editorState = EditorState.create({
+  doc: "function multiply(a, b) {\n  return a*b\n}\nmultiply(2, 2)",
   extensions
 })
 
@@ -20,19 +21,27 @@ const editor = new EditorView({
   state: editorState
 })
 
-const consoleState = EditorState.create({
-  extensions
-})
-
-const consoleEditor = new EditorView({
-  parent: document.getElementById('console'),
-  state: consoleState
-})
-
-const runButton = document.getElementById('runButton');
-
 runButton.addEventListener('click', () => {
-  window.console.log('Interpreting');
-
-  console.contentDOM = "asdasd"
+  const result = evaluate(editor.state.doc.toString())
+  document.getElementById('console').innerText = result
 });
+
+function emptyEditor(view) {
+  let text = view.state.doc.toString()
+  let changes = []
+  for (let next = 0; next < text.length; next++) {
+    changes.push({from: next, to: next + 1})
+  }
+  view.dispatch({changes})
+}
+
+function evaluate(str:string): String {
+  let result = ''
+  try {
+    result = eval(str)
+  } catch (e) {
+    console.log(e)
+    result = e + '\n' + e.stack
+  }
+  return new String(result)
+}
