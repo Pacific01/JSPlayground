aa356b53ef8339fd651eb4cf10a84ec22fe6ff29
Jaime Pascual Torres <jpascual@everlyrusher.com>
2022-10-16 13:56:35 +0200

    Some css and layout

--- index.ts
+++ index.ts
@@ -1,8 +1,20 @@
 import {basicSetup, EditorView} from "codemirror"
 import {javascript} from "@codemirror/lang-javascript"
 
-new EditorView({
-  doc: "console.log('hello')\n",
+const editor = new EditorView({
   extensions: [basicSetup, javascript()],
   parent: document.getElementById('editor')
 })
+
+const console = new EditorView({
+  extensions: [basicSetup, javascript()],
+  parent: document.getElementById('console')
+})
+
+const runButton = document.getElementById('runButton');
+
+runButton.addEventListener('click', () => {
+  window.console.log('Interpreting');
+
+  console.contentDOM = "asdasd"
+});
